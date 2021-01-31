93470c5d57acfe58cc2ac4732ddb260243e6a2cf
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-01-30 22:42:26 -0800

    Fix dash particles

--- scenes/effects_animations/dash_effect_animation.gd
+++ scenes/effects_animations/dash_effect_animation.gd
@@ -7,7 +7,7 @@ const SCALE_MULTIPLIER := 0.09
 
 func calc_position(player, horizontal_sign: int) -> Vector2:
     var x_offset: float = -horizontal_sign * player._get_radius()
-    return player.position + Vector2(x_offset, 0) + OFFSET_DEFAULT * horizontal_sign
+    return player.position + Vector2(x_offset, 0) + OFFSET_DEFAULT * Vector2(horizontal_sign, 1)
 
 func calc_scale(player) -> float:
     return player._get_radius() * SCALE_MULTIPLIER
